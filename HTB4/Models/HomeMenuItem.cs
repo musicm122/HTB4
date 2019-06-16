7932ed26d483e2ce57987c1a0e894b2abb42df74
Terrance Smith <Terrance.Smith@cdpehs.com>
2019-06-15 20:12:20 -0400

    Split Menu Item enum type out from home menu items.

--- HTB4/Models/HomeMenuItem.cs
+++ HTB4/Models/HomeMenuItem.cs
@@ -4,19 +4,6 @@ using System.Text;
 
 namespace HTB4.Models
 {
-    public enum MenuItemType
-    {
-        About,
-        CaseDrain,
-        Cylinder,
-        Pump,
-        Motor,
-        MotorTorque,
-        ConversionTable1,
-        ConversionTable2,
-        ConversionTable3,
-        Debug,
-    }
     public class HomeMenuItem
     {
         public MenuItemType Id { get; set; }
---
+++ HTB4/Models/MenuItemType.cs
@@ -0,0 +1,24 @@
+﻿namespace HTB4.Models
+{
+    public enum MenuItemType
+    {
+        About,
+
+        CaseDrain,
+        CaseDrainMenu,
+        CaseDrainGpm,
+
+        CylinderMenu,
+        CylinderForce,
+        CylinderSpeed,
+        CylinderPsi,
+
+        Pump,
+        Motor,
+        MotorTorque,
+        ConversionTable1,
+        ConversionTable2,
+        ConversionTable3,
+        Debug,
+    }
+}
